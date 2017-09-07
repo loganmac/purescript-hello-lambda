90cbb2760ae4339b2e9c22268e86396bc295d401
Logan McPhail <lmcphail@mb12-1835.local>
2017-09-07 16:47:23 -0600

    update to do something more interesting

--- src/Lambda.purs
+++ src/Lambda.purs
@@ -6,10 +6,19 @@ import Data.Either (Either(..))
 import Simple.JSON (readJSON, writeJSON)
 
 type Event =
+  { name :: String }
+
+type Response =
   { greeting :: String }
 
 handler :: String -> {statusCode :: String, body:: String}
 handler event = do
   case runExcept $ readJSON event of
-    Right (x :: Event) -> {statusCode: "200", body: writeJSON x}
-    Left e -> {statusCode: "400", body: """{"error": "Couldn't parse JSON."}"""}
+    Right (x :: Event) ->
+      { statusCode: "200"
+      , body: writeJSON { greeting : "Hello, " <> x.name <> "!" }
+      }
+    Left e ->
+      { statusCode: "400"
+      , body: """{"error": "Couldn't parse JSON."}"""
+      }
